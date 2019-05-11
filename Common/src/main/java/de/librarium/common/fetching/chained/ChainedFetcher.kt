10678af394cdf7ff14a904dc00af7038c4ad993e
I-Al-Istannen <i-al-istannen@users.noreply.github.com>
2019-05-11 20:43:43 +0200

    Add a chained fetcher that tries multiple fetcher in the supplied order

---
+++ Common/src/main/java/de/librarium/common/fetching/chained/ChainedFetcher.kt
@@ -0,0 +1,28 @@
+package de.librarium.common.fetching.chained
+
+import de.librarium.common.BookMetadata
+import de.librarium.common.fetching.BookFetcher
+
+/**
+ * A fetcher that just calls multiple fetcher in order and picks the first one.
+ */
+class ChainedFetcher(private val fetcher: List<BookFetcher>) : BookFetcher {
+
+    override fun fetch(isbn: String): BookMetadata? {
+        return fetcher.asSequence()
+            .mapNotNull { it.fetch(isbn) }
+            .firstOrNull()
+    }
+
+    override fun fetchUrl(url: String): BookMetadata? {
+        return fetcher.asSequence()
+            .mapNotNull { it.fetchUrl(url) }
+            .firstOrNull()
+    }
+
+    override fun search(query: String): List<String> {
+        return fetcher.asSequence()
+            .mapNotNull { it.search(query) }
+            .first()
+    }
+}
